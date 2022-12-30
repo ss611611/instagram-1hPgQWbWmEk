245a1df3d93be9c232ef03b949c140eb20120b73
ss611611 <ss611611@gmail.com>
2022-12-30 18:51:13 +0800

    [feat] add react-navigation/stack

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShow: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
